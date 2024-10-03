78be6ff08813d3185a5ec8036245eedecd9e1c71
Varun Choudhary <143994029+VarunGithub9@users.noreply.github.com>
2024-10-03 22:47:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';  // <-- Import axios
+import axios from 'axios'; 
 import Header from './components/Header';
 import styled from 'styled-components';
 import MovieComponent from './components/MovieComponent';
